c4e36cfebe0c18f28afcd122d3f64a42d2dde105
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-02-10 17:24:22 -0700

    2/10 setup v3

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -24,12 +24,35 @@ return (
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
-    {/* Form will go here */}
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title: </label>
+        <input
+          id="title"
+          name="title"
+          value={newBook.title}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="author">Author: </label>
+        <input
+          id="author"
+          name="author"
+          value={newBook.author}
+          onChange={handleInputChange}
+        />
+
+        <button type="submit">Add Book</button>
+      </form>
   </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+  <div className="bookCardsDiv">{books.map((book, index) => (
+        <div key={index} className="bookCard">
+          <h4>{book.title}</h4>
+          <p>by {book.author}</p>
+        </div>
+  ))}
+  </div>
 </div>
 );
-
 };
 
 export default Bookshelf;
